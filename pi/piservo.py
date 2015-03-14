8c743a33375229ce6b6a3f89fa8f097193f176f2
Paul Kremser <paul.kremser@cern.ch>
2015-03-14 15:56:01 +0100

    minor fixes and simple tak-a-pic funtionality

--- pi/piservo.py
+++ pi/piservo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import optparse, os, time, sys
+import os, time, sys
 import optparse, ConfigParser
 import paramiko
 
@@ -9,7 +9,7 @@ class pyservo:
         config = ConfigParser.ConfigParser()
         config.read(cfg_file)
         self.servo_cmd = "echo %s=" % config.get("servo", "channel") + "%s%% > /dev/servoblaster"
-        
+        self.image_folder = config.get("pi", "image_folder")
         # Dyanmic range of our servos
         range_min, range_max = int(config.get("servo", "range_min")), int(config.get("servo", "range_max"))
         self.dyn_range = range_max - range_min
@@ -32,24 +32,38 @@ class pyservo:
         ranged_percent = (self.dyn_range / 100.) * percent + self.range_min
         print self.servo_cmd%ranged_percent
         ssh_stdin, ssh_stdout, ssh_stderr = self.ssh_shell.exec_command(self.servo_cmd%ranged_percent)
+        
+    def takePicture(self, filename):
+        self.ssh_shell.exec_command("/root/snap.py %s" %  (os.path.join(self.image_folder, filename), ))
+        
+        
 
 def main():
     p = optparse.OptionParser(description="PiServo Control")
     p.add_option("-c", "--config", dest="cfg", help="The servo config file", default = 'x_axis.cfg')
+    p.add_option("-a", "--angle", dest="angle", help="The Servo angle in percent of dynamic range", default = '-1')
+    p.add_option("-i", "--image", dest="img", help="Image filename to be taken", default = '')
+    
+    
     (opts, args) = p.parse_args()
     # Sanity
     try:
         cfgfile = open(opts.cfg, 'r')
+        angle = int(opts.angle)
     except Exception as e:
         p.print_help()
         print e.__doc__
         return -1
 
     p = pyservo(opts.cfg)
-    for i in range(10):
-        p.move(80)
-        time.sleep(.5)
-        p.move(20)
-        time.sleep(.5)
+    if len(opts.img) > 0:
+        p.takePicture(opts.img)
+    else:
+        if angle > 0: p.move(angle)
+        else:
+            for i in range(10):
+                p.move(i*10)
+                time.sleep(.5)
+                #~ p.takePicture("moved_%.2i.jpg"%i)
 
 if (__name__ == "__main__"): main()
